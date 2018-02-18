311c35dd42f08b1a0f64965c9deb454a580a3541
Max.Nova <max@silviaterra.com>
2018-02-18 11:30:40 -0800

    Parse kindle highlights

---
+++ parse_highlights.py
@@ -0,0 +1,19 @@
+import argparse
+import pandas as pd
+
+
+def parse(fn, out_fn):
+    df = pd.read_csv(fn, header=7)  # starts on line 7
+    with open(out_fn, 'w') as f:
+        for note in df['Annotation']:
+            f.write(note + '\n\n')
+
+    return out_fn
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Extract Kindle highlights')
+    parser.add_argument('-k', '--kindle_highlights', type=str)
+    parser.add_argument('-o', '--out_fn', default='/tmp/highlights.txt')
+    args = parser.parse_args()
+    print parse(args.kindle_highlights, args.out_fn)
